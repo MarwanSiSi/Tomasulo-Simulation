1c0e87bcde6ec240564713155ccd96458c9ee1cc
Abdelrahman Zidan <abdelrahman.azdn>
2024-12-13 19:12:49 +0200

    reservation stations initialised in simulator

--- backend/src/classes/Simulator.py
+++ backend/src/classes/Simulator.py
@@ -1,10 +1,12 @@
 from collections import deque
 
+from backend.src.enums.Opcode import Opcode
+
 from .CDB import CDB
 from .Instruction import Instruction
 from .MemoryManager import MemoryManager
 from .RegisterFile import RegisterFile
-from .Station.Station import Station
+from .Station.Station import Station, AddStation, MulStation, LoadStation, StoreStation
 
 
 class Simulator:
@@ -22,6 +24,11 @@ class Simulator:
         self.memory_manager: MemoryManager = MemoryManager()
         self.register_file: RegisterFile = RegisterFile()
         self.reservation_stations: list[Station] = []
+        self.reservation_stations.append(AddStation("Add/Sub Int Station", 1, 1, "AI"))
+        self.reservation_stations.append(AddStation("Add/Sub Float Station", 1, 1, "AF"))
+        self.reservation_stations.append(MulStation("Mul/Div Float Station", 1, 1, "M"))
+        self.reservation_stations.append(LoadStation("Load Station", 1, 1, "L"))
+        self.reservation_stations.append(StoreStation("Store Station", 1, 1, "S"))
         self.cdb = CDB()
         self.instruction_queue: deque[Instruction] = deque()
 
@@ -43,5 +50,19 @@ class Simulator:
         if len(self.instruction_queue) == 0:
             return
 
-        instruction = self.instruction_queue.popleft()
+        instruction = self.instruction_queue[0]
+
+        if instruction.opcode in {Opcode.DADDI,Opcode.DSUBI}:
+            result = self.reservation_stations[0].assign_entry(instruction, self.cycle)
+        elif instruction.opcode in {Opcode.ADD_D, Opcode.ADD_S, Opcode.SUB_D, Opcode.SUB_S}:
+            result = self.reservation_stations[1].assign_entry(instruction, self.cycle)
+        elif instruction.opcode in {Opcode.MUL_D, Opcode.MUL_S, Opcode.DIV_D, Opcode.DIV_S}:
+            result = self.reservation_stations[2].assign_entry(instruction, self.cycle)
+        elif instruction.opcode in {Opcode.LW, Opcode.LD, Opcode.L_S, Opcode.L_D}:
+            result = self.reservation_stations[3].assign_entry(instruction, self.cycle)
+        elif instruction.opcode in {Opcode.SW, Opcode.SD, Opcode.S_S, Opcode.S_D}:
+            result = self.reservation_stations[4].assign_entry(instruction, self.cycle)
+
+        if result:
+            self.instruction_queue.popleft()
         raise NotImplementedError
