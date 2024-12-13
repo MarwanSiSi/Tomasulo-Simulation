8e910fec2532c3bbb9cc1baaaf8524a38da74b19
MoGamal <mohamedgamal0420@gmail.com>
2024-12-13 23:08:46 +0200

    submit config

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -53,7 +53,16 @@ function App() {
   console.log(intAddSubStations);
 
   // things to be sent on mount : addSubLatency, mulDivLatency, intAddSubLatency, loadBufferSize, storeBufferSize, floatAddSubStationSize, floatMulDivStationSize, intAddSubStationSize, cacheHitLatency, cacheMissLatency, cacheSize, blockSize
-  useEffect(() => {}, []);
+  useEffect(() => {
+    axios
+      .post("http://0.0.0.0:8080/config", config)
+      .then((response) => {
+        console.log("Config posted successfully:", response.data);
+      })
+      .catch((error) => {
+        console.error("Error posting config:", error);
+      });
+  }, []);
 
   return (
     <div className="p-6 max-w-full mx-auto">
